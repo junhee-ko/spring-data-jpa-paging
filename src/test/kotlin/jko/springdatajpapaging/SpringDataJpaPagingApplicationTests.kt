926d27f9bf3eae4f693a146b9ade143a887c6440
junhee-ko <junheee.ko@gmail.com>
2022-02-13 13:39:30 +0900

    first commit

---
+++ src/test/kotlin/jko/springdatajpapaging/SpringDataJpaPagingApplicationTests.kt
@@ -0,0 +1,13 @@
+package jko.springdatajpapaging
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDataJpaPagingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
